d5eba9f82961be5b9f2b42df752399c9bcc9f82a
Mike Bell <mdb036@gmail.com>
2024-02-03 01:04:20 +0000

    Initial commit

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2023 Your Name
+ * Copyright (c) 2024 Michael Bell
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_MichaelBell_tinyQV (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,81 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = ui_in; // Placeholder
+
+  // Bidirs are used for SPI interface
+  wire [3:0] spi_data_in = {uio_in[5:4], uio_in[2:1]};
+  wire [3:0] spi_data_out;
+  wire [3:0] spi_data_oe;
+  assign uio_out[5:4] = spi_data_out[3:2];
+  assign uio_out[2:1] = spi_data_out[1:0];
+  assign uio_oe = {2'b11, spi_data_oe[3:2], 1'b1, spi_data_oe[1:0], 1'b1};
+
+  // CPU to memory controller wiring
+  wire [23:1] instr_addr;
+  wire        instr_fetch_restart;
+  wire        instr_fetch_stall;
+  wire        instr_fetch_started;
+  wire        instr_fetch_stopped;
+  wire [15:0] instr_data;
+  wire        instr_ready;
+  wire [27:0] data_addr;
+  wire  [1:0] data_write_n;
+  wire  [1:0] data_read_n;
+  wire [31:0] data_to_write;
+  wire        data_ready;
+  wire [31:0] data_from_read;
+
+  tinyqv_cpu cpu(
+        .clk(clk),
+        .rstn(rst_n),
+
+        .instr_addr(instr_addr),
+        .instr_fetch_restart(instr_fetch_restart),
+        .instr_fetch_stall(instr_fetch_stall),
+
+        .instr_fetch_started(instr_fetch_started),
+        .instr_fetch_stopped(instr_fetch_stopped),
+        .instr_data_in(instr_data),
+        .instr_ready(instr_ready),
+
+        .data_addr(data_addr),
+        .data_write_n(data_write_n),
+        .data_read_n(data_read_n),
+        .data_out(data_to_write),
+
+        .data_ready(data_ready),
+        .data_in(data_from_read)
+    );
+
+  tinyqv_mem_ctrl mem(
+        .clk(clk),
+        .rstn(rst_n),
+
+        .instr_addr(instr_addr),
+        .instr_fetch_restart(instr_fetch_restart),
+        .instr_fetch_stall(instr_fetch_stall),
+
+        .instr_fetch_started(instr_fetch_started),
+        .instr_fetch_stopped(instr_fetch_stopped),
+        .instr_data(instr_data),
+        .instr_ready(instr_ready),
+
+        .data_addr(data_addr),
+        .data_write_n(data_write_n),
+        .data_read_n(data_read_n),
+        .data_to_write(data_to_write),
+
+        .data_ready(data_ready),
+        .data_from_read(data_from_read),
+
+        .spi_data_in(spi_data_in),
+        .spi_data_out(spi_data_out),
+        .spi_data_oe(spi_data_oe),
+        .spi_select_flash(uio_out[0]),
+        .spi_select_ram_a(uio_out[6]),
+        .spi_select_ram_b(uio_out[7]),
+        .spi_clk_out(uio_out[3])
+    );
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -25,8 +25,7 @@ async def test_adder(dut):
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
   dut.ui_in.value = 20
-  dut.uio_in.value = 30
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 50
+  assert dut.uo_out.value == 20
