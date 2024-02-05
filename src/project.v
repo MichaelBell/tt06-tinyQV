897e22d1343a2c7e43ad7113d2a38326a8b70758
Mike Bell <mdb036@gmail.com>
2024-02-05 22:26:29 +0000

    WIP

--- src/project.v
+++ src/project.v
@@ -8,90 +8,149 @@
 module tt_um_MichaelBell_tinyQV (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
+/*verilator lint_off UNUSEDSIGNAL*/
+    input  wire [7:0] uio_in,   // IOs: Input path - only some bits used
+/*verilator lint_on UNUSEDSIGNAL*/
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+/*verilator lint_off UNUSEDSIGNAL*/
     input  wire       ena,
+/*verilator lint_on UNUSEDSIGNAL*/
     input  wire       clk,
     input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in; // Placeholder
+    // Register the reset on the negative edge of clock for safety.
+    // This also allows the option of async reset in the design, which might be preferable in some cases
+    reg rst_reg_n;
+    always @(negedge clk) rst_reg_n <= rst_n;
 
-  // Bidirs are used for SPI interface
-  wire [3:0] spi_data_in = {uio_in[5:4], uio_in[2:1]};
-  wire [3:0] spi_data_out;
-  wire [3:0] spi_data_oe;
-  assign uio_out[5:4] = spi_data_out[3:2];
-  assign uio_out[2:1] = spi_data_out[1:0];
-  assign uio_oe = {2'b11, spi_data_oe[3:2], 1'b1, spi_data_oe[1:0], 1'b1};
+    // Bidirs are used for SPI interface
+    wire [3:0] qspi_data_in = {uio_in[5:4], uio_in[2:1]};
+    wire [3:0] qspi_data_out;
+    wire [3:0] qspi_data_oe;
+    wire       qspi_clk_out;
+    wire       qspi_flash_select;
+    wire       qspi_ram_a_select;
+    wire       qspi_ram_b_select;
+    assign uio_out = {qspi_ram_b_select, qspi_ram_a_select, qspi_data_out[3:2], 
+                      qspi_clk_out, qspi_data_out[1:0], qspi_flash_select};
+    assign uio_oe = rst_n ? {2'b11, qspi_data_oe[3:2], 1'b1, qspi_data_oe[1:0], 1'b1} : 8'h00;
 
-  // CPU to memory controller wiring
-  wire [23:1] instr_addr;
-  wire        instr_fetch_restart;
-  wire        instr_fetch_stall;
-  wire        instr_fetch_started;
-  wire        instr_fetch_stopped;
-  wire [15:0] instr_data;
-  wire        instr_ready;
-  wire [27:0] data_addr;
-  wire  [1:0] data_write_n;
-  wire  [1:0] data_read_n;
-  wire [31:0] data_to_write;
-  wire        data_ready;
-  wire [31:0] data_from_read;
+    wire [27:0] addr;
+    wire  [1:0] write_n;
+    wire  [1:0] read_n;
+/*verilator lint_off UNUSEDSIGNAL*/
+    wire [31:0] data_to_write;  // Currently only bottom byte used.
+/*verilator lint_on UNUSEDSIGNAL*/
 
-  tinyqv_cpu cpu(
+    wire        data_ready;
+    reg [31:0] data_from_read;
+
+    tinyQV i_tinyqv(
         .clk(clk),
-        .rstn(rst_n),
+        .rstn(rst_reg_n),
 
-        .instr_addr(instr_addr),
-        .instr_fetch_restart(instr_fetch_restart),
-        .instr_fetch_stall(instr_fetch_stall),
-
-        .instr_fetch_started(instr_fetch_started),
-        .instr_fetch_stopped(instr_fetch_stopped),
-        .instr_data_in(instr_data),
-        .instr_ready(instr_ready),
-
-        .data_addr(data_addr),
-        .data_write_n(data_write_n),
-        .data_read_n(data_read_n),
+        .data_addr(addr),
+        .data_write_n(write_n),
+        .data_read_n(read_n),
         .data_out(data_to_write),
 
         .data_ready(data_ready),
-        .data_in(data_from_read)
+        .data_in(data_from_read),
+
+        .spi_data_in(qspi_data_in),
+        .spi_data_out(qspi_data_out),
+        .spi_data_oe(qspi_data_oe),
+        .spi_clk_out(qspi_clk_out),
+        .spi_flash_select(qspi_flash_select),
+        .spi_ram_a_select(qspi_ram_a_select),
+        .spi_ram_b_select(qspi_ram_b_select)
     );
 
-  tinyqv_mem_ctrl mem(
+    // Peripheral IOs on ui_in and uo_out
+/*verilator lint_off UNUSEDSIGNAL*/
+    wire [1:0] interrupt = ui_in[1:0];  // TODO
+    wire       spi_miso  = ui_in[2];    // TODO
+/*verilator lint_on UNUSEDSIGNAL*/
+    wire       uart_rxd  = ui_in[3];
+
+    wire       spi_cs = 1'b1;  // TODO
+    wire       spi_sck = 1'b1;
+    wire       spi_mosi = 1'b1;
+    wire       spi_dc = 1'b0;
+    wire       uart_txd;
+    wire       uart_rts;
+    reg  [1:0] gpio_out;
+
+    assign uo_out[0] = spi_cs;
+    assign uo_out[1] = spi_sck;
+    assign uo_out[2] = spi_mosi;
+    assign uo_out[3] = spi_dc;
+    assign uo_out[4] = uart_txd;
+    assign uo_out[5] = uart_rts;
+    assign uo_out[7:6] = gpio_out;
+
+    // Address to peripheral map
+    localparam PERI_NONE = 0;
+    localparam PERI_GPIO_OUT = 2;
+    localparam PERI_GPIO_IN = 3;
+    localparam PERI_UART = 4;
+    localparam PERI_UART_STATUS = 5;
+
+    reg [2:0] connect_peripheral;
+
+    always @(*) begin
+        if (addr == 28'h8000000) connect_peripheral = PERI_GPIO_OUT;
+        else if (addr == 28'h8000004) connect_peripheral = PERI_GPIO_IN;
+        else if (addr == 28'h8000010) connect_peripheral = PERI_UART;
+        else if (addr == 28'h8000014) connect_peripheral = PERI_UART_STATUS;
+        else connect_peripheral = PERI_NONE;
+    end
+
+    // All transactions complete immediately
+    assign data_ready = 1'b1;
+
+    // Read data
+    always @(*) begin
+        case (connect_peripheral)
+            PERI_GPIO_OUT:    data_from_read = {24'h0, uo_out};
+            PERI_GPIO_IN:     data_from_read = {24'h0, ui_in};
+            PERI_UART:        data_from_read = {24'h0, uart_rx_data};
+            PERI_UART_STATUS: data_from_read = {30'h0, uart_rx_valid, uart_tx_busy};
+            default:          data_from_read = 32'hFFFF_FFFF;
+        endcase
+    end
+
+    // GPIO Out
+    always @(posedge clk) begin
+        if (!rst_reg_n) gpio_out <= 0;
+        if (write_n != 2'b11 && connect_peripheral == PERI_GPIO_OUT) gpio_out <= data_to_write[1:0];
+    end
+
+    // UART
+    wire uart_tx_busy;
+    wire uart_rx_valid;
+    wire [7:0] uart_rx_data;
+    wire uart_tx_start = write_n != 2'b11 && connect_peripheral == PERI_UART;
+
+    uart_tx #(.CLK_HZ(66_000_000), .BIT_RATE(115_200)) i_uart_tx(
         .clk(clk),
-        .rstn(rst_n),
+        .resetn(rst_reg_n),
+        .uart_txd(uart_txd),
+        .uart_tx_en(uart_tx_start),
+        .uart_tx_data(data_to_write[7:0]),
+        .uart_tx_busy(uart_tx_busy) 
+    );
 
-        .instr_addr(instr_addr),
-        .instr_fetch_restart(instr_fetch_restart),
-        .instr_fetch_stall(instr_fetch_stall),
-
-        .instr_fetch_started(instr_fetch_started),
-        .instr_fetch_stopped(instr_fetch_stopped),
-        .instr_data(instr_data),
-        .instr_ready(instr_ready),
-
-        .data_addr(data_addr),
-        .data_write_n(data_write_n),
-        .data_read_n(data_read_n),
-        .data_to_write(data_to_write),
-
-        .data_ready(data_ready),
-        .data_from_read(data_from_read),
-
-        .spi_data_in(spi_data_in),
-        .spi_data_out(spi_data_out),
-        .spi_data_oe(spi_data_oe),
-        .spi_flash_select(uio_out[0]),
-        .spi_ram_a_select(uio_out[6]),
-        .spi_ram_b_select(uio_out[7]),
-        .spi_clk_out(uio_out[3])
+    uart_rx #(.CLK_HZ(66_000_000), .BIT_RATE(115_200)) i_uart_rx(
+        .clk(clk),
+        .resetn(rst_reg_n),
+        .uart_rxd(uart_rxd),
+        .uart_rts(uart_rts),
+        .uart_rx_read(connect_peripheral == PERI_UART && read_n != 2'b11),
+        .uart_rx_valid(uart_rx_valid),
+        .uart_rx_data(uart_rx_data) 
     );
 
 endmodule
